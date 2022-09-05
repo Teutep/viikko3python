b3f00600e2d1b746972346a72858d13e4333b358
Teuvo <teuvo.almgren@gmail.com>
2022-09-05 12:27:48 +0300

    Viikko 3 teht

---
+++ teht5.1.py
@@ -0,0 +1,7 @@
+#Arpakuutioiden kvanti- ja kvaliteetti
+import random
+Arpakuutio = int(input("Kuinka monta arpakuutiota?\n"))
+Arvo = 0
+for x in range(0, Arpakuutio):
+    Arvo += random.randint(1, 6)
+print(f"Heitettyjä noppia on {Arpakuutio} ja näiden noppien arvo on {Arvo}")
