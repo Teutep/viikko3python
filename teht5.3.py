77784009114fdeb64a6885be4ff8278c3b6dea79
Teuvo <teuvo.almgren@gmail.com>
2022-09-05 15:26:38 +0300

    Viikko 3 teht

--- teht5.3.py
+++ teht5.3.py
@@ -1,13 +1,17 @@
 Kokonaisluku = int(input("Anna kokonaisluku: "))
+Lista = []
 n = 2
 found = 0
 while found == 0:
     if Kokonaisluku % n == 0 and n != Kokonaisluku:
         found = 1
         print(f"Luku {Kokonaisluku} ei ole alkuluku")
-    elif n == 100:
+        nm = Kokonaisluku % n
+    elif n == 10:
         print(f"Luku {Kokonaisluku} on alkuluku")
+        nm = Kokonaisluku % n
         break
     else:
         n += 1
-print("Toimenpide on ohi")
+        nm = Kokonaisluku % n
+print(f"Toimenpide on ohi")
